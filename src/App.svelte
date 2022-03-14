282f6419e632f7b5ccf86952996f3f3b387b83e9
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-03-14 18:55:10 +0800

    Add sass

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-	export let name: string;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Arum</h1>
+	<p>A beginner friendly scripting language</p>
 </main>
 
-<style>
+<style lang="scss">
 	main {
 		text-align: center;
 		padding: 1em;
