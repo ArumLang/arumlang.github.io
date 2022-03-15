b16860a8f374bd0eb20acb57cb758d1887b9489d
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-03-15 10:45:03 +0800

    Tried to add a download button

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,12 +3,15 @@
 	<h1>
 		<img src="https://cdn.discordapp.com/attachments/948382155455725621/953095963067695164/arum.png" alt="ArumLogo" />
 	</h1>
+	<button>
+		Download
+	</button>
 </header>
 
 <style lang="scss">
 	header {
 		background-color: #333;
-		padding-left: 17.5rem;
+		// padding-left: 17.5rem;
 	}
 
 	h1 {
@@ -18,5 +21,6 @@
 
 	img {
 		max-width: 7.5rem;
+		margin-left: 3rem;
 	}
 </style>
