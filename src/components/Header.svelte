258b0e95aa410b42511f7d716028974ca46f1bb9
ofsho <ofekbendavid9@gmail.com>
2022-03-24 11:50:02 +0200

    improved header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,12 +1,12 @@
-<header>
+<header style="height: 7rem;">
 	<!-- TODO: switch CDN from discord lmao -->
-	<h1>
-		<img src="https://cdn.discordapp.com/attachments/948382155455725621/953095963067695164/arum.png" alt="ArumLogo" />
-		<div id="wrapper" style="float: right; display: inline-block;">
-			<button style="max-width: 1rem, height:2rem; text-align: center;" onclick="location.href='https://discord.gg/gCGmraBRQ8'">
+	<h1 style="display: flex; justify-content: space-around">
+		<img src="/favicon.png" alt="ArumLogo" />
+		<div id="wrapper" style="float: right; display: inline-block; display: flex; justify-content: center">
+			<button style="max-width: 1rem, height:4rem; text-align: center;" onclick="location.href='https://discord.gg/gCGmraBRQ8'">
 				Discord
 			</button>
-			<button style="max-width: 1rem, height:2rem; text-align: center;" onclick="location.href='https://github.com/ArumLang'">
+			<button style="max-width: 1rem, height:4rem; text-align: center;" onclick="location.href='https://github.com/ArumLang'">
 				Github
 			</button>
 		</div>
